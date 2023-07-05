cdb988be42a0f51acc00321e401c06e6f4befb94
Costanza <60357882+costikid@users.noreply.github.com>
2023-07-05 18:17:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-    // JavaScript to change testimonials in the hero section every 4 seconds
+#    // JavaScript to change testimonials in the hero section every 4 seconds
     const testimonials = document.querySelectorAll(".testimonial");
     let currentTestimonialIndex = 0;
 
@@ -109,6 +109,16 @@ modalClose.addEventListener('click', () => {
 });
 
 
-
-
-
+$(function(){
+  $('ul.tabs li:first').addClass('active');
+  $('.block article').hide();
+  $('.block article:first').show();
+  $('ul.tabs li').on('click',function(){
+    $('ul.tabs li').removeClass('active');
+    $(this).addClass('active')
+    $('.block article').hide();
+    var activeTab = $(this).find('a').attr('href');
+    $(activeTab).show();
+    return false;
+  });
+})
